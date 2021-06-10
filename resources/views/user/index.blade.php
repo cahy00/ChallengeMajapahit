354a466b258c5b6e5da9ccb73d06f4e82cdcb2da
Cahyo <cariercahyo@gmail.com>
2021-06-10 23:40:18 +0800

    #

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,5 +1,17 @@
 @extends('templates.main')
 
+@section('style')
+<style>
+	.container{
+		margin-bottom: 50px;
+		font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif
+
+	}
+
+	
+</style>
+@endsection
+
 @section('content')
 
 <div class="container">
@@ -9,7 +21,29 @@
 				<div class="card-header bg-primary">
 					<h3 class="card-title">Data</h3>
 				</div>
-				<div class="card-body">ini user</div>
+				<div class="card-body">
+					<table class="table table-bordered">
+						<thead>
+							<tr>
+								<td>No</td>
+								<td>Nama</td>
+								<td>Points</td>
+								<td>Action</td>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>1</td>
+								<td>Cahyo</td>
+								<td>50</td>
+								<td>
+									<a href="" class="btn btn-danger">Edit</a>
+									<a href="" class="btn btn-warning">Delete</a>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
 			</div>
 		</div>
 	</div>
