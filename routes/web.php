8a191fd3ac64329bfb336e3b7e905d55ebcec283
Cahyo <cariercahyo@gmail.com>
2021-06-10 23:26:18 +0800

    #

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('templates.main');
 });
+
+/**
+ * Route User
+ */
+Route::get('/user', [UserController::class, 'index'])->name('user.index');
