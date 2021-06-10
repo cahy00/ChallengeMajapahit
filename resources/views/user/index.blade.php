289a851ee71d6a091e2e5241dd064c9b2cddbf4d
Cahyo <cariercahyo@gmail.com>
2021-06-10 23:29:25 +0800

    #

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,13 +1,16 @@
 @extends('templates.main')
 
 @section('content')
-<div class="row">
-	<div class="col-md">
-		<div class="card">
-			<div class="card-header bg-primary">
-				<h3 class="card-title">Data</h3>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md">
+			<div class="card">
+				<div class="card-header bg-primary">
+					<h3 class="card-title">Data</h3>
+				</div>
+				<div class="card-body">ini user</div>
 			</div>
-			<div class="card-body">ini user</div>
 		</div>
 	</div>
 </div>
