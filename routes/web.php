e4038a8f018ecb6f49ff78626c3e5775f4d15a3c
Cahyo <cariercahyo@gmail.com>
2021-06-11 14:17:48 +0800

    #

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\MerchandiseController;
 
 Route::get('/', function () {
@@ -20,3 +21,11 @@ Route::get('/user', [UserController::class, 'index'])->name('user.index');
  * *Route Barang
  */
 Route::get('/merchandise', [MerchandiseController::class, 'index'])->name('merchandise.index');
+
+
+/**
+ * *Route Transaction
+ */
+Route::get('/transaction', [TransactionController::class, 'index'])->name('transaction.index');
+Route::get('/transaction/create', [TransactionController::class, 'create'])->name('transaction.create');
+
