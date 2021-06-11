c7a052c8963542680fb0057f495822e3e971361e
Cahyo <cariercahyo@gmail.com>
2021-06-11 14:58:03 +0800

    #

--- database/migrations/2021_06_11_065342_create_transactions_table.php
+++ database/migrations/2021_06_11_065342_create_transactions_table.php
@@ -15,6 +15,8 @@ class CreateTransactionsTable extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
+						$table->unsignedBigInteger('user_id');
+						$table->unsignedBigInteger('merchandise_id');
             $table->timestamps();
         });
     }
