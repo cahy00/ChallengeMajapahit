9a66eb607b90e1162f2a2cd5d007fa5ae17d1f4d
Cahyo <cariercahyo@gmail.com>
2021-06-11 14:41:16 +0800

    #

---
+++ resources/views/transaction/create.blade.php
@@ -0,0 +1,68 @@
+@extends('templates.main')
+
+@section('style')
+<style>
+	.container{
+		margin-bottom: 10px;
+
+	}
+
+	
+</style>
+@endsection
+
+@section('content')
+
+<div class="container">
+	<div class="row">
+		<div class="col-md">
+			<div class="card">
+				<div class="card-header bg-primary">
+					<h3 class="card-title">User</h3>
+				</div>
+				<div class="card-body">
+					<form action="">
+						<div class="row">
+							<div class="col-md-6">
+
+								<div class="form-group">
+									<label for="">User</label>
+									<select name="user_id" id="" class="form-control">
+										<option value="">Cahyo</option>
+									</select>
+								</div>
+
+							</div>
+							<div class="col-md-6">
+
+								<div class="form-group">
+									<label for="">Barang</label>
+									<select name="merchandise_id" id="" class="form-control">
+										<option value="">Komputer</option>
+									</select>
+								</div>
+
+							</div>
+						</div>
+						<br>
+						<table class="table">
+							<tr>
+								<td>Harga</td>
+								<td>Rp 20000000</td>
+							</tr>
+							<tr>
+								<td>Potongan</td>
+								<td>Rp 0</td>
+							</tr>
+						</table>
+
+						<button type="submit" class="btn btn-primary">Process</button>
+					</form>
+				<div class="card-footer">
+					{{-- {{$data->links()}} --}}
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
---
+++ resources/views/transaction/index.blade.php
@@ -0,0 +1,57 @@
+@extends('templates.main')
+
+@section('style')
+<style>
+	.container{
+		margin-bottom: 10px;
+
+	}
+
+	
+</style>
+@endsection
+
+@section('content')
+
+<div class="container">
+	<div class="row">
+		<div class="col-md">
+			<div class="card">
+				<div class="card-header bg-primary">
+					<h3 class="card-title">User</h3>
+				</div>
+				<div class="card-body">
+					<table class="table table-bordered">
+						<thead>
+							<tr>
+								<td>No</td>
+								<td>Nama Barang</td>
+								<td>Harga</td>
+								<td>Stock</td>
+								<td>Action</td>
+							</tr>
+						</thead>
+						<tbody>
+							@foreach ($data as $item)
+							<tr>
+								<td>{{ $loop->iteration + ($data->perPage() * ($data->currentPage() - 1)) }}</td>
+								<td>{{ $item->name }}</td>
+								<td>{{ $item->price }}</td>
+								<td>{{ $item->stock }}</td>
+								<td>
+									<a href="" class="btn btn-warning">Edit</a>
+									<a href="" class="btn btn-danger">Delete</a>
+								</td>
+							</tr>
+							@endforeach
+						</tbody>
+					</table>
+				</div>
+				<div class="card-footer">
+					{{$data->links()}}
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
