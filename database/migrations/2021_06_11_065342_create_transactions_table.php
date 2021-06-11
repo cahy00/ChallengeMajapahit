febd2f66c0118894c246b9ff571840798f26cdba
Cahyo <cariercahyo@gmail.com>
2021-06-11 14:59:27 +0800

    #

--- database/migrations/2021_06_11_065342_create_transactions_table.php
+++ database/migrations/2021_06_11_065342_create_transactions_table.php
@@ -17,6 +17,7 @@ class CreateTransactionsTable extends Migration
             $table->id();
 						$table->unsignedBigInteger('user_id');
 						$table->unsignedBigInteger('merchandise_id');
+						$table->enum('status', ['success', 'pending', 'fail']);
             $table->timestamps();
         });
     }
