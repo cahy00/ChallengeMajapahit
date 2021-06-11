f809d276f802b475f310051665e499498e4a772b
Cahyo <cariercahyo@gmail.com>
2021-06-11 14:19:23 +0800

    #

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -13,7 +13,7 @@ class TransactionController extends Controller
      */
     public function index()
     {
-        //
+        return view('transaction.index');
     }
 
     /**
@@ -23,7 +23,7 @@ class TransactionController extends Controller
      */
     public function create()
     {
-        //
+        return view('transaction.create');
     }
 
     /**
