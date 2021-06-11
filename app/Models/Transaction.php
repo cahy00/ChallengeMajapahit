88e6c1f0b308861e4ed06c0f1223b08edec937e2
Cahyo <cariercahyo@gmail.com>
2021-06-11 14:57:07 +0800

    #

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model
 {
     use HasFactory;
+		protected $fillable = [
+			'user_id', 'merchandise_id', 'status'
+		];
+
+		public function user()
+		{
+				return $this->belongsTo(User::class);
+		}
+
+		public function merchandise()
+		{
+				return $this->belongsTo(Merchandise::class);
+		}
 }
