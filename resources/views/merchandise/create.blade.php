893e47b438bb5e41e29785b173bfd0dbdfbd9798
Cahyo <cariercahyo@gmail.com>
2021-06-12 20:40:50 +0800

    #

--- resources/views/merchandise/create.blade.php
+++ resources/views/merchandise/create.blade.php
@@ -23,21 +23,26 @@
 					<form action="{{route('user.store')}}" method="POST">
 						@csrf
 						<div class="row">
-							<div class="col-md-6">
+							<div class="col-md-4">
 								<div class="form-group">
 									<label for="">Nama Barang</label>
 									<input type="text" name="name" class="form-control {{$errors->first('name') ? 'is-invalid' : ''}}" placeholder="Masukkan name">
 									<span class="error invalid-feedback">{{$errors->first('name')}}</span>
 								</div>
+							</div>
+							<div class="col-md-4">
 								<div class="form-group">
 									<label for="">Price</label>
 									<input type="number" name="price" class="form-control {{$errors->first('price') ? 'is-invalid' : ''}}" placeholder="Masukkan price">
 									<span class="error invalid-feedback">{{$errors->first('price')}}</span>
 								</div>
-								<div class="form-group">
-									<label for="">Stock</label>
-									<input type="number" name="stock" class="form-control {{$errors->first('stock') ? 'is-invalid' : ''}}" placeholder="Masukkan stock">
-									<span class="error invalid-feedback">{{$errors->first('stock')}}</span>
+							</div>
+								<div class="col-md-4">
+									<div class="form-group">
+										<label for="">Stock</label>
+										<input type="number" name="stock" class="form-control {{$errors->first('stock') ? 'is-invalid' : ''}}" placeholder="Masukkan stock">
+										<span class="error invalid-feedback">{{$errors->first('stock')}}</span>
+									</div>
 								</div>
 							</div>
 							
