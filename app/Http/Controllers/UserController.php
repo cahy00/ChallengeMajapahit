588b43d330d8f44bacd8da8ac2aab21a2a84bd6e
Cahyo <cariercahyo@gmail.com>
2021-06-12 19:11:47 +0800

    #

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -93,7 +93,11 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $user = User::findOrFail($id);
+				$user->update([
+					'email' => $request->email,
+					'name' => $request->name
+				]);
     }
 
     /**
