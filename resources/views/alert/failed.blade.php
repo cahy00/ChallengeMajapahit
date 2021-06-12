620927ac73c41d85d51ab87b7a29fd1ff509b136
Cahyo <cariercahyo@gmail.com>
2021-06-12 22:51:30 +0800

    #

--- resources/views/alert/failed.blade.php
+++ resources/views/alert/failed.blade.php
@@ -1,6 +1,6 @@
-@if (session('success'))
-		<div class="alert alert-success alert-dismissible">
+@if (session('failed'))
+		<div class="alert alert-danger alert-dismissible">
 			<button class="close" data-dismiss="alert" area-hidden="true" type="button">x</button>
-			{{session('success')}}
+			{{session('failed')}}
 		</div>
 @endif
