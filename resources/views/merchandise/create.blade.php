dfc3b3771448b05b1d7409c9a07f5f2c2c29d743
Cahyo <cariercahyo@gmail.com>
2021-06-12 20:36:59 +0800

    #

---
+++ resources/views/merchandise/create.blade.php
@@ -0,0 +1,54 @@
+@extends('templates.main')
+
+@section('style')
+<style>
+	.container{
+		margin-bottom: 10px;
+
+	}
+
+	
+</style>
+@endsection
+
+@section('content')
+<div class="container">
+	<div class="row">
+		<div class="col-md">
+			<div class="card">
+				<div class="card-header bg-primary">
+					<h3 class="card-title">User Create</h3>
+				</div>
+				<div class="card-body">
+					<form action="{{route('user.store')}}" method="POST">
+						@csrf
+						<div class="row">
+							<div class="col-md-6">
+								<div class="form-group">
+									<label for="">Email</label>
+									<input type="email" name="email" class="form-control {{$errors->first('email') ? 'is-invalid' : ''}}" placeholder="Masukkan Email">
+									<span class="error invalid-feedback">{{$errors->first('email')}}</span>
+								</div>
+								<div class="form-group">
+									<label for="">Password</label>
+									<input type="password" name="password" class="form-control {{$errors->first('password') ? 'is-invalid' : ''}}" placeholder="Masukkan Password">
+									<span class="error invalid-feedback">{{$errors->first('password')}}</span>
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="form-group">
+									<label for="">Nama Lengkap</label>
+									<input type="text" name="name" class="form-control {{$errors->first('name') ? 'is-invalid' : ''}}" placeholder="Masukkan Nama">
+									<span class="error invalid-feedback">{{$errors->first('name')}}</span>
+								</div>
+							</div>
+						</div>
+						<br>
+						<button type="submit" class="btn btn-primary">Simpan</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
