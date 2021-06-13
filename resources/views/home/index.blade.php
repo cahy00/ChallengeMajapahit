9b373a7af43c5296f6b27f8a71d6673fabb9c1d4
Cahyo <cariercahyo@gmail.com>
2021-06-13 13:13:47 +0800

    #

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,27 @@
+
+@extends('templates.main')
+
+@section('title')
+		Barang
+@endsection
+
+@section('style')
+<style>
+	.container{
+		margin-bottom: 10px;
+
+	}
+
+	
+</style>
+@endsection
+
+@section('content')
+<div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+@endsection
