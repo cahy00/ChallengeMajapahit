134851ecf07641cb81139be91a7c93fb10aeff7a
Cahyo <cariercahyo@gmail.com>
2021-06-13 16:08:18 +0800

    #

---
+++ resources/views/user/show.blade.php
@@ -0,0 +1,21 @@
+@extends('templates.main')
+
+@section('title')
+		Hadiah
+@endsection
+
+
+@section('style')
+<style>
+	.container{
+		margin-bottom: 10px;
+
+	}
+
+	
+</style>
+@endsection
+
+@section('content')
+
+@endsection
